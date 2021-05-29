d5ce68de8620529cd505ba5728126c3e2223b6d2
James <james@salweensolutions.com>
2021-05-29 22:38:26 +0200

    fix(navigation): change title name and header name

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Event from '../views/Event';
+import EventDetail from '../views/Event/sub-view/EventDetail';
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="event">
-      <Stack.Screen name="event" component={Event} />
       <Stack.Screen
-        name="eventDetail"
+        name="event"
         component={Event}
         options={({route}) => ({
-          title: route.params.name,
+          title: 'EVENTS',
+        })}
+      />
+      <Stack.Screen
+        name="eventDetail"
+        component={EventDetail}
+        options={({route}) => ({
+          title: route.params.title,
+          headerBackTitle: ' ',
         })}
       />
     </Stack.Navigator>
